bf0fb011718a430eb81905ddcb6ad7f7857f8857
qoxogus <a01034367055@gmail.com>
2021-06-30 14:16:07 +0900

    [ADD] keyUtil 추가

--- src/main/java/com/server/MemberManagement/util/KeyUtil.java
+++ src/main/java/com/server/MemberManagement/util/KeyUtil.java
@@ -1,2 +1,30 @@
-package com.server.MemberManagement.util;public class KeyUtil {
+package com.server.MemberManagement.util;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Random;
+
+@Service
+public class KeyUtil {
+
+    protected int size;
+
+    public String getKey(int size) {
+        this.size = size;
+        return getAuthCode();
+    }
+
+    //Generate Authentication Code for RandomNumbers
+    private String getAuthCode() {
+        Random random = new Random();
+        StringBuffer buffer = new StringBuffer();
+        int num = 0;
+
+        while(buffer.length() < size) {
+            num = random.nextInt(10);
+            buffer.append(num);
+        }
+
+        return buffer.toString();
+    }
 }
