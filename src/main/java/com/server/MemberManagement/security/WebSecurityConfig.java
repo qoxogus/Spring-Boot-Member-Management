9a95b6620a6b48943244af1dc07af8897bd65f66
qoxogus <a01034367055@gmail.com>
2021-07-02 07:39:48 +0900

    [UPDATE] security change

--- src/main/java/com/server/MemberManagement/security/WebSecurityConfig.java
+++ src/main/java/com/server/MemberManagement/security/WebSecurityConfig.java
@@ -41,7 +41,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/v1/signup").permitAll()//
                 .antMatchers("/v1/refreshtoken").permitAll()//
                 .antMatchers("/v1/email").permitAll()//
-                .antMatchers("/v1/verify/email").permitAll()//
+                .antMatchers("/v1/verify-email").permitAll()//
                 .antMatchers("/v1/password-change").permitAll()//
                 .antMatchers("/v1/user/**").hasRole("USER")//
                 .antMatchers("/h2-console/**/**").permitAll()
