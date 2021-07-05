cbc7d8fe8938750ef030d1ceb451352ed590cd27
qoxogus <a01034367055@gmail.com>
2021-07-05 11:11:20 +0900

    [UPDATE] delete

--- src/main/java/com/server/MemberManagement/service/BoardServiceImpl.java
+++ src/main/java/com/server/MemberManagement/service/BoardServiceImpl.java
@@ -56,7 +56,7 @@ public class BoardServiceImpl implements BoardService {
 
     @Override
     public String deleteBoard(Long id) {
-        Board board = boardRepository.findById(id)
+        boardRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 존재하지 않습니다."));
 
         boardRepository.deleteById(id);
