e0885ecbc13efe5a423b0342267d2bbd9633e8cc
qoxogus <a01034367055@gmail.com>
2021-09-07 16:46:32 +0900

    [ADD] CSRF 설정, dockerfile

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM openjdk:11
+ARG JAR_FILE=build/libs/*.jar
+COPY ${JAR_FILE} app.jar
+ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom","-jar","/app.jar"]
--- src/main/java/com/server/MemberManagement/config/WebConfig.java
+++ src/main/java/com/server/MemberManagement/config/WebConfig.java
@@ -5,7 +5,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 public class WebConfig implements WebMvcConfigurer {
-    
+
     @Override //CORS 설정
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") //모든 요청에 대하여
